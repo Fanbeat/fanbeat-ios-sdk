9d2dd4753655bac980b05591f2de064ed0a69167
Tony Sullivan <tony.sullivan@fanbeat.com>
2016-08-11 08:13:11 -0500

    production S3 bucket

--- FanBeat/Classes/FBConstants.h
+++ FanBeat/Classes/FBConstants.h
@@ -12,7 +12,7 @@
 #define FANBEAT_APP_URI_SCHEME @"ingame://"
 #define FANBEAT_STORE_ID 966632826
 #define FANBEAT_SDK_PLIST_KEY @"fanbeat_id"
-#define FANBEAT_BASE_S3_URL @"https://s3.amazonaws.com/ingame-test/partners/"
+#define FANBEAT_BASE_S3_URL @"https://s3.amazonaws.com/ingame-prod/partners/"
 #define FANBEAT_BRANCH_LIVE_KEY @"key_live_oam5GSs8U81sJ8TvPo8v6bbpDudyMBQN"
 #define FANBEAT_BRANCH_TEST_KEY @"key_test_mbo9SGq4LZXBQ9HvTj89lgcgzvnwJDN0"
 
